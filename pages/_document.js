a0ab27cd961032faae5a1208ecb1d9dbdc1f5914
Mohamed Fouad HANANI <info.fouadhanani@gmail.com>
2021-09-02 11:06:28 +0100

    excluded google analytics from dev env

--- pages/_document.js
+++ pages/_document.js
@@ -9,26 +9,26 @@ export default class MyDocument extends Document {
 				<Head>
 					<link rel="manifest" href="/manifest.json" />
 
-					{/* {process.env.NODE_ENV === 'PRODUCTION' && ( */}
-					<>
-						<script
-							async
-							src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
-						/>
-						<script
-							dangerouslySetInnerHTML={{
-								__html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${GA_TRACKING_ID}', {
-              page_path: window.location.pathname,
-            });
-          `,
-							}}
-						/>
-					</>
-					{/* )} */}
+					{process.env.NODE_ENV !== 'development' && (
+						<>
+							<script
+								async
+								src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+							/>
+							<script
+								dangerouslySetInnerHTML={{
+									__html: `
+	window.dataLayer = window.dataLayer || [];
+	function gtag(){dataLayer.push(arguments);}
+	gtag('js', new Date());
+	gtag('config', '${GA_TRACKING_ID}', {
+		page_path: window.location.pathname,
+	});
+`,
+								}}
+							/>
+						</>
+					)}
 				</Head>
 				<body>
 					<Main />
